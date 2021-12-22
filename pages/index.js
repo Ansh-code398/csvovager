0ef6b5099b44c001993be1448f3b6a0e211fb28b
Ansh Sharma <neetusharma981074@gmail.com>
2021-12-22 20:14:32 +0530

    m

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ const index = () => {
                 <title>Home</title>
             </Head>
             <h1 className="text-white">In Development</h1>
+            <iframe width={700} height={400} src="https://96afa406.sibforms.com/serve/MUIEAOYTzFb4IN5QdKIHqSpuQ_lRwYZs4sLXfKieEhCM4G3Iyzb5aMAZJ1upDU3skC1JlMutqsg1WjwE9QaLbOhuJZS54vYlpschtlNIob897TSFQjhaTccFSKvZTLpW7uh5Bzk6-f07fm37REImlpmNPB9a5QkZefcn92P5Mr2y1OawIhu4odjjcd4yyIqI0EMuF-zlnTO8ql4M" frameBorder={0} scrolling="auto" allowFullScreen style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%'}} />
+  
         </div>
     )
 }
