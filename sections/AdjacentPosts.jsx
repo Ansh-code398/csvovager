abfac4592567a723c318583a1f089ba60c9a0e6d
Ansh Sharma <neetusharma981074@gmail.com>
2021-12-22 11:28:29 +0530

    m

--- sections/AdjacentPosts.jsx
+++ sections/AdjacentPosts.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-import { AdjacentPostCard } from '../components';
-import { getAdjacentPosts } from '../services';
+import { AdjacentPostCard } from '../pages/components/index.js';
+import { getAdjacentPosts } from '../services/index.js';
 
 const AdjacentPosts = ({ createdAt, slug }) => {
   const [adjacentPost, setAdjacentPost] = useState(null);
--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
-import { FeaturedPostCard } from '../components';
-import { getFeaturedPosts } from '../services';
+import { FeaturedPostCard } from '../pages/components/index.js';
+import { getFeaturedPosts } from '../services/index.js';
 
 const responsive = {
   superLargeDesktop: {
