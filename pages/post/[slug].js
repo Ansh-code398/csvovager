8d0810d5b55eda428c7b8a10ad2e6f518363f93b
Ansh Sharma <neetusharma981074@gmail.com>
2021-12-24 19:04:59 +0530

    m

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -29,7 +29,6 @@ const PostDetails = ({ post }) => {
             <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
             {/* <CommentsForm slug={post.slug} />
             <Comments slug={post.slug} /> */}
-            <iframe width={700} height={400} src="https://96afa406.sibforms.com/serve/MUIEAOYTzFb4IN5QdKIHqSpuQ_lRwYZs4sLXfKieEhCM4G3Iyzb5aMAZJ1upDU3skC1JlMutqsg1WjwE9QaLbOhuJZS54vYlpschtlNIob897TSFQjhaTccFSKvZTLpW7uh5Bzk6-f07fm37REImlpmNPB9a5QkZefcn92P5Mr2y1OawIhu4odjjcd4yyIqI0EMuF-zlnTO8ql4M" frameBorder={0} scrolling="auto" allowFullScreen style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%'}} />
           </div>
           <div className="col-span-1 lg:col-span-4">
             <div className="relative lg:sticky top-8">
