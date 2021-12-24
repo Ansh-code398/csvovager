954cd77e6f72ac043466a45729057494ffdc34e6
Ansh Sharma <neetusharma981074@gmail.com>
2021-12-24 19:03:46 +0530

    m

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Script from 'next/script';
 import React from 'react';
 import Header from './Header';
+import Link from 'next/link';
 
 const Layout = ({ children }) => (
   <>
@@ -10,6 +11,26 @@ const Layout = ({ children }) => (
   </Head>
     <Header />
     {children}
+    {/* Footer */}
+    <footer className="bg-bookmark-blue py-8">
+                <div className="container flex flex-col md:flex-row items-center">
+                    <div className="flex flex-1 flex-wrap items-center justify-center md:justify-start gap-12">
+                        <img src="./img/logo.png" className="w-8 h-8" alt="" />
+                        <ul className="flex text-white uppercase gap-12 text-xs kex-wrap">
+                            <Link className="cursor-pointer" href='/'>Home</Link>
+                            <Link className="cursor-pointer" href='/blog'>Blog</Link>
+                            <Link className="cursor-pointer" href='/#subscribe'>Subscribe</Link>
+                        </ul>
+                    </div>
+                    <div className="flex gap-10 mt-12 md:mt-0">
+                        
+                        <p className="cursor-pointer text-white text-2xl">
+                            Copyright &copy; 2021 CS Voyager
+                        </p>
+                        
+                    </div>
+                </div>
+            </footer>
     <Script src="https://kit.fontawesome.com/8f366c7ba6.js" crossOrigin="anonymous"></Script>
   </>
 );
--- pages/index.js
+++ pages/index.js
@@ -514,26 +514,6 @@ const index = () => {
                     </div>
                 </div>
             </section>
-            {/* Footer */}
-            <footer className="bg-bookmark-blue py-8">
-                <div className="container flex flex-col md:flex-row items-center">
-                    <div className="flex flex-1 flex-wrap items-center justify-center md:justify-start gap-12">
-                        <img src="./img/logo.png" className="w-8 h-8" alt="" />
-                        <ul className="flex text-white uppercase gap-12 text-xs kex-wrap">
-                            <Link className="cursor-pointer" href='/'>Home</Link>
-                            <Link className="cursor-pointer" href='/blog'>Blog</Link>
-                            <Link className="cursor-pointer" href='/#subscribe'>Subscribe</Link>
-                        </ul>
-                    </div>
-                    <div className="flex gap-10 mt-12 md:mt-0">
-                        
-                        <p className="cursor-pointer text-white text-2xl">
-                            Copyright &copy; 2021 CS Voyager
-                        </p>
-                        
-                    </div>
-                </div>
-            </footer>
         </div>
     );
 }
